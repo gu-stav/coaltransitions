a9b940634843c53cd45489d7bc0ce28c6e5ecc0e
Gustav Pursche <pursche@posteo.de>
2019-04-23 15:14:37 +0200

    Fix build

--- src/pages/publications/publication-list/index.jsx
+++ src/pages/publications/publication-list/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Publication from './publication';
 import style from './style';
 
-export default ({ publications }) => (
+export default ({ publications = [] }) => (
   <ul>
     <style jsx>{style}</style>
 
