b990209e51e60b9b34661cdfd4adec41b1cc81a4
Gustav Pursche <pursche@posteo.de>
2019-04-24 13:40:44 +0200

    Does the URL manipulation break SSR?

--- src/pages/publications/index.jsx
+++ src/pages/publications/index.jsx
@@ -33,9 +33,6 @@ const Page = ({
     publications: { nodes: publications }
   }
 }) => {
-  const urlAuthors = getFilterFromUrl('authors');
-  const urlTags = getFilterFromUrl('keywords');
-  const urlRange = getFilterFromUrl('range', year => parseInt(year, 10));
   const tags = extractPublicationsTags(publications);
   const [
     minPublicationYear,
@@ -44,46 +41,95 @@ const Page = ({
 
   // eslint-disable-next-line no-unused-vars
   const [filter, setFilter] = useState({
-    authors: urlAuthors || [],
-    tags: urlTags || [],
-    range: urlRange || [minPublicationYear, maxPublicationYear]
+    authors: [],
+    tags: [],
+    range: [minPublicationYear, maxPublicationYear]
   });
 
-  // Keep state in sync with the URL
-  useEffect(() => {
-    setUrlForFilter('authors', filter.authors);
-    setUrlForFilter('keywords', filter.tags);
-
-    // Only set the URL parameter, if the start and beginning are different
-    // than the default
-    if (
-      filter.range[0] !== minPublicationYear ||
-      filter.range[1] !== maxPublicationYear
-    ) {
-      setUrlForFilter('range', filter.range);
-    } else {
-      setUrlForFilter('range', null);
-    }
+  const filteredPublications = filterPublications(publications, {
+    authors: filter.authors,
+    tags: filter.tags,
+    range: filter.range
   });
-
-  let filteredPublications = publications;
-
-  if (
-    filter.authors.length > 0 ||
-    filter.tags.length > 0 ||
-    filter.range[0] !== minPublicationYear ||
-    filter.range[1] !== maxPublicationYear
-  ) {
-    filteredPublications = filterPublications(publications, {
-      authors: filter.authors,
-      tags: filter.tags,
-      range: filter.range
-    });
-  }
 
   return (
     <>
       <Helmet title="Publications" />
+
+      <Constraint>
+        <h2>Filter</h2>
+
+        <h3>Author</h3>
+        <Select
+          options={extractPublicationsAuthors(publications)}
+          value={filter.authors.map(author => ({
+            value: author,
+            label: author
+          }))}
+          onChange={selected => {
+            const authors = selected.map(({ value }) => value);
+
+            setFilter(state => ({
+              ...state,
+              authors
+            }));
+          }}
+          isMulti
+          isSearchable
+        />
+
+        <h3>Keywords</h3>
+        <Select
+          options={tags}
+          value={filter.tags.map(tag => ({
+            value: tag,
+            label: tags.find(({ value }) => value === tag).label
+          }))}
+          onChange={selected => {
+            const selectedTags = selected.map(({ value }) => value);
+
+            setFilter(state => ({
+              ...state,
+              tags: selectedTags
+            }));
+          }}
+          isMulti
+          isSearchable
+        />
+
+        <h3>Year of publication</h3>
+        <Range
+          min={minPublicationYear}
+          max={maxPublicationYear}
+          value={filter.range}
+          onChange={value => {
+            setFilter(state => ({
+              ...state,
+              range: value
+            }));
+          }}
+          allowCross={false}
+          dots
+          marks={generateRangeMarks(minPublicationYear, maxPublicationYear)}
+        />
+
+        <br />
+        <br />
+
+        <button
+          type="button"
+          onClick={() => {
+            setFilter(state => ({
+              ...state,
+              authors: [],
+              tags: [],
+              range: [minPublicationYear, maxPublicationYear]
+            }));
+          }}
+        >
+          Reset filter
+        </button>
+      </Constraint>
 
       <h1>Publications ({filteredPublications.length})</h1>
 
