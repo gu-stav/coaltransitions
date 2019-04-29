3578ab18cd967471324b44f6dae9b39e52d57fe9
Gustav Pursche <pursche@posteo.de>
2019-04-29 16:01:02 +0200

    Fix Schema for falsy authors

--- gatsby-node.js
+++ gatsby-node.js
@@ -17,6 +17,15 @@ exports.onCreateNode = ({ node }) => {
   }
 
   if (
+    (node.internal.type === 'wordpress__acf_publications' ||
+      node.internal.type === 'wordpress__wp_publications') &&
+    node.acf.author === false
+  ) {
+    // eslint-disable-next-line no-param-reassign
+    node.acf.author = [];
+  }
+
+  if (
     node.internal.type === 'wordpress__acf_publications' ||
     node.internal.type === 'wordpress__wp_publications'
   ) {
