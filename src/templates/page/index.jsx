c6005edb35e4d2dd8aaa53024e258cd7ca48227e
Gustav Pursche <pursche@posteo.de>
2019-06-04 11:48:31 +0200

    Add page templates

--- src/templates/page/index.jsx
+++ src/templates/page/index.jsx
@@ -1,3 +1,61 @@
+import { graphql } from 'gatsby';
+import Helmet from 'react-helmet';
 import React from 'react';
 
-export default () => <>Page!</>;
+import Constraint from '../../components/constraint';
+import Intro from '../../components/intro';
+import Richtext from '../../components/richtext';
+import withLayout from '../../components/with-layout';
+
+const Page = ({
+  data: {
+    page: {
+      title,
+      acf: { content, intro }
+    }
+  }
+}) => (
+  <>
+    <Helmet title={title} />
+
+    <article>
+      <Constraint topLevel>
+        <h1 dangerouslySetInnerHTML={{ __html: title }} />
+
+        <Intro intro={intro} />
+
+        {content &&
+          content.map(block => {
+            const { __typename: type } = block;
+
+            // eslint-disable-next-line default-case
+            switch (type) {
+              case 'WordPressAcf_text':
+                return <Richtext content={block.text} />;
+            }
+
+            return null;
+          })}
+      </Constraint>
+    </article>
+  </>
+);
+
+export default withLayout(Page);
+
+export const query = graphql`
+  query($wordpressId: Int) {
+    page: wordpressPage(wordpress_id: { eq: $wordpressId }) {
+      title
+      acf {
+        content: content_page {
+          __typename
+
+          ... on WordPressAcf_text {
+            text
+          }
+        }
+      }
+    }
+  }
+`;
