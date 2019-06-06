47a3f40334fed912c457fa983063f0a88edb9fb0
Gustav Pursche <pursche@posteo.de>
2019-06-06 17:36:21 +0200

    ResearchProjectList: enhance date range display

--- src/components/research-projects-list/research-project/index.jsx
+++ src/components/research-projects-list/research-project/index.jsx
@@ -4,32 +4,41 @@ import ArrowRightIcon from '../../../../static/icons/arrow-alt-right.svg';
 import Button from '../../button';
 import style, { buttonIcon } from './style';
 
-export default ({ title, acf: { start, end, summary, externalLink } }) => (
-  <div className="project">
-    <style jsx>{style}</style>
-    {buttonIcon.styles}
+export default ({ title, acf: { start, end, summary, externalLink } }) => {
+  let range = `${start}`;
 
-    <div className="duration-container">
-      <p className="duration">
-        {start} – {end}
-      </p>
+  if (end && start !== end) {
+    range += ` – ${end}`;
+  }
+
+  return (
+    <div className="project">
+      <style jsx>{style}</style>
+      {buttonIcon.styles}
+
+      <div className="duration-container">
+        <p className="duration">{range}</p>
+      </div>
+
+      <h3 className="title-container">
+        <a
+          href={externalLink}
+          className="title"
+          dangerouslySetInnerHTML={{ __html: title }}
+        />
+      </h3>
+
+      <div className="content-container">
+        <div
+          dangerouslySetInnerHTML={{ __html: summary }}
+          className="summary"
+        />
+
+        <Button to="/publications/" theme="blue">
+          Publications
+          <ArrowRightIcon className={buttonIcon.className} />
+        </Button>
+      </div>
     </div>
-
-    <h3 className="title-container">
-      <a
-        href={externalLink}
-        className="title"
-        dangerouslySetInnerHTML={{ __html: title }}
-      />
-    </h3>
-
-    <div className="content-container">
-      <div dangerouslySetInnerHTML={{ __html: summary }} className="summary" />
-
-      <Button to="/publications/" theme="blue">
-        Publications
-        <ArrowRightIcon className={buttonIcon.className} />
-      </Button>
-    </div>
-  </div>
-);
+  );
+};
