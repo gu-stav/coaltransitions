099237a3fbc6e9a977aca155c0a97616534a25c8
Gustav Pursche <pursche@posteo.de>
2019-06-06 12:10:31 +0200

    Richtext: improve list styling

--- src/components/richtext/style.js
+++ src/components/richtext/style.js
@@ -1,6 +1,6 @@
 import css from 'styled-jsx/css';
 
-import { mixins, mq } from '../../token';
+import { mixins, mq, colors } from '../../token';
 
 export default css`
   :global(.richtext) {
@@ -28,6 +28,26 @@ export default css`
   }
 
   :global(.richtext ul) {
+    list-style: none;
     padding-left: 1rem;
+  }
+
+  :global(.richtext li) {
+    position: relative;
+  }
+
+  :global(.richtext li:before) {
+    color: ${colors.blueBrand};
+    content: '·';
+    font-size: 1.75rem;
+    left: -0.75rem;
+    line-height: 1;
+    position: absolute;
+    top: 0.25rem;
+    vertical-align: middle;
+  }
+
+  :global(.richtext li + li) {
+    margin-top: 1rem;
   }
 `;
