44044c6d1777c467ed0bbffd2e641708d7c3733d
Gustav Pursche <pursche@posteo.de>
2019-06-11 14:43:18 +0200

    AuthorList/ TagList: sort items by name (Fixes #13)

--- src/components/tag-list/index.jsx
+++ src/components/tag-list/index.jsx
@@ -7,18 +7,20 @@ export default ({ tags, trim = false, onFilter = false }) => (
   <ul>
     <style jsx>{style}</style>
 
-    {tags.map(({ name, slug }, index) => {
-      if (trim !== false && trim <= index) {
-        return null;
-      }
+    {tags
+      .sort(({ name: a }, { name: b }) => a.localeCompare(b))
+      .map(({ name, slug }, index) => {
+        if (trim !== false && trim <= index) {
+          return null;
+        }
 
-      return (
-        <li key={`tag-${slug}`}>
-          <Tag slug={slug} onFilter={onFilter}>
-            {name}
-          </Tag>
-        </li>
-      );
-    })}
+        return (
+          <li key={`tag-${slug}`}>
+            <Tag slug={slug} onFilter={onFilter}>
+              {name}
+            </Tag>
+          </li>
+        );
+      })}
   </ul>
 );
