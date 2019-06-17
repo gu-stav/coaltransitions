814526d9886a6cf67a8346106c080c62a3f33edc
Gustav Pursche <pursche@posteo.de>
2019-06-17 14:29:57 +0200

    Picture: simplify component & fail early

--- src/components/picture/index.jsx
+++ src/components/picture/index.jsx
@@ -2,18 +2,24 @@ import React from 'react';
 
 import style from './style';
 
-export default ({ image, type = 'fluid', className, ...rest }) => (
-  <picture {...rest}>
-    <style jsx>{style}</style>
+export default ({ image, type = 'fluid', className, ...rest }) => {
+  const imageByType = image.childImageSharp[type];
 
-    <source type="image/webp" srcSet={image.childImageSharp[type].srcSetWebp} />
-    <source type="image/png" srcSet={image.childImageSharp[type].srcSet} />
+  if (!imageByType) {
+    return null;
+  }
 
-    {/* eslint-disable-next-line jsx-a11y/alt-text */}
-    <img
-      src={image.childImageSharp[type].src}
-      loading="lazy"
-      className={className}
-    />
-  </picture>
-);
+  const { srcSetWebp, srcSet, src } = imageByType;
+
+  return (
+    <picture {...rest}>
+      <style jsx>{style}</style>
+
+      <source type="image/webp" srcSet={srcSetWebp} />
+      <source type="image/png" srcSet={srcSet} />
+
+      {/* eslint-disable-next-line jsx-a11y/alt-text */}
+      <img src={src} loading="lazy" className={className} />
+    </picture>
+  );
+};
