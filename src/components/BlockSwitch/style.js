f903800e5e9fe00e3b2b2424d4263184e402e373
Gustav Pursche <pursche@posteo.de>
2020-10-01 11:07:28 +0200

    Partner: prevent image-container from growing

--- src/components/BlockSwitch/style.js
+++ src/components/BlockSwitch/style.js
@@ -3,13 +3,13 @@ import css from 'styled-jsx/css';
 import { mq } from '../../token';
 
 export default css`
-  :global(.block-content figure) {
+  :global(.block-content > figure) {
     margin-bottom: 1.5rem;
     margin-top: 1.5rem;
   }
 
   @media ${mq.tablet} {
-    :global(.block-content figure) {
+    :global(.block-content > figure) {
       margin-bottom: 3.5rem;
       margin-top: 3.5rem;
     }
